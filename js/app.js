2fb7eabc74202d5e57111ddb41b008993a9429bb
Kenold Beauplan <bkenold@gmail.com>
2019-09-19 10:05:32 -0400

    update data

--- js/app.js
+++ js/app.js
@@ -1,76 +1,76 @@
 var podcast = {
     "episodes": [
         {
-        "id": 1,
-        "title": "Samantha Colas Miss Haiti Univers 2018 nan Chokarella",
-        "description": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
-        "date": "11/4/2018",
-        "duration": 28,
-        "file_path": 68
-        }, {
-        "id": 2,
-        "title": "MechansT ap prezante premye albòm li nan chokarella",
-        "description": "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
-        "date": "1/21/2018",
-        "duration": 21,
-        "file_path": 65
-        }, {
-        "id": 3,
-        "title": "Wanito reyaktive. L’ap rakonte rezon absans li sou sèn nan",
-        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
-        "date": "8/23/2018",
-        "duration": 20,
-        "file_path": 92
-        }, {
-        "id": 4,
-        "title": "Trouble Boy ap prezante nouvo albòm li nan The Morning Show",
-        "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
-        "date": "9/30/2018",
-        "duration": 18,
-        "file_path": 53
-        }, {
-        "id": 5,
-        "title": "Atis brezilyen Amaral TiBlan nan Chokarella",
-        "description": "In congue. Etiam justo. Etiam pretium iaculis justo.",
-        "date": "6/30/2018",
-        "duration": 9,
-        "file_path": 68
-        }, {
-        "id": 6,
-        "title": "Carel Pedre x Pascal Milien",
-        "description": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
-        "date": "7/1/2018",
-        "duration": 16,
-        "file_path": 89
-        }, {
-        "id": 7,
-        "title": "Buteur haitien",
-        "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
-        "date": "3/25/2019",
-        "duration": 27,
-        "file_path": 56
-        }, {
-        "id": 8,
-        "title": " Chokarella Interview",
-        "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
-        "date": "6/2/2019",
-        "duration": 9,
-        "file_path": 53
-        }, {
-        "id": 9,
-        "title": "Riva Precil & Monvelyno nan Chokarella pou pale de patisipasyon yo nan Carifesta",
-        "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-        "date": "6/1/2018",
-        "duration": 20,
-        "file_path": 81
-        }, {
-        "id": 10,
-        "title": "Ti Joe Zenny ap Prezante Albòm Invictus Kreyol la Nan Chokarella",
-        "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
-        "date": "8/17/2018",
-        "duration": 15,
-        "file_path": 93
-        }
+            "id": 1,
+            "title": "est donec odio justo sollicitudin ut suscipit a feugiat et eros vestibulum",
+            "description": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+            "date": "4/27/2019",
+            "duration": 37,
+            "file_path": 51
+          }, {
+            "id": 2,
+            "title": "amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices",
+            "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+            "date": "5/29/2019",
+            "duration": 31,
+            "file_path": 60
+          }, {
+            "id": 3,
+            "title": "diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in faucibus",
+            "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+            "date": "8/21/2019",
+            "duration": 41,
+            "file_path": 84
+          }, {
+            "id": 4,
+            "title": "adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit",
+            "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+            "date": "7/15/2019",
+            "duration": 5,
+            "file_path": 54
+          }, {
+            "id": 5,
+            "title": "eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in faucibus",
+            "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.",
+            "date": "9/12/2019",
+            "duration": 33,
+            "file_path": 75
+          }, {
+            "id": 6,
+            "title": "luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce",
+            "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+            "date": "4/3/2019",
+            "duration": 13,
+            "file_path": 88
+          }, {
+            "id": 7,
+            "title": "lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus",
+            "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+            "date": "2/18/2019",
+            "duration": 10,
+            "file_path": 61
+          }, {
+            "id": 8,
+            "title": "aenean sit amet justo morbi ut odio cras mi pede malesuada in imperdiet",
+            "description": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+            "date": "9/9/2019",
+            "duration": 54,
+            "file_path": 58
+          }, {
+            "id": 9,
+            "title": "eros suspendisse accumsan tortor quis turpis sed ante vivamus tortor duis mattis",
+            "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+            "date": "2/21/2019",
+            "duration": 38,
+            "file_path": 96
+          }, {
+            "id": 10,
+            "title": "potenti in eleifend quam a odio in hac habitasse platea dictumst",
+            "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+            "date": "6/8/2019",
+            "duration": 23,
+            "file_path": 64
+          }
     ]
 }
 
